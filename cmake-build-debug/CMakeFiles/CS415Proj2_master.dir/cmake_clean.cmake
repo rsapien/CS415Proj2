5dea9ca8b5bb0c282550c7fbcda53b74e1cbccd5
rsapien <sapien@sonoma.edu>
2020-03-18 21:47:51 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/CS415Proj2_master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS415Proj2_master.dir/main.cpp.o"
+  "CS415Proj2_master"
+  "CS415Proj2_master.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS415Proj2_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
