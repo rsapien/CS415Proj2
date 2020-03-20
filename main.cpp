6e423266d3077cbe8af315f01658686e7d8d02c6
rsapien <sapien@sonoma.edu>
2020-03-19 18:28:26 -0700

    - Added struct - Added function skeletons - Started file reader in main()

--- main.cpp
+++ main.cpp
@@ -3,10 +3,38 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int main() {
+struct Client{
+    int sDay, eDay, wtp;
+};
 
+void getData(vector<Client>& Clients, ifstream& dFile){
+
+}
+
+void output(){
+
+}
+
+int main() {
+    vector<Client> Clients;
+
+    // Getting data file
+    ifstream inFile;
+    string fName;
+    cout << "Enter the file to read the data: "; cin >> fName; cout << endl;
+
+    inFile.open(fName);
+    if(!inFile){
+        cout << "Unable to open file.\nExiting..." << endl;
+        exit(1);
+    }
+
+    getData(Clients, inFile);
+    inFile.close();
     return 0;
 }
